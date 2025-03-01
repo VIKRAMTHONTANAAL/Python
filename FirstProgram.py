1193389d0ed15ee9d2c1a9edd0d43a3ccaddf248
vikrammthontanaal <thontanaalvikram@gmail.com>
2025-03-01 13:05:17 +0530

    Sum and other

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,1 +1,36 @@
 print("Hello World")
+print("This is Vikram", "I am programmer")
+print(23)
+print(48+22)
+name = "Vikram"
+age = 28
+price = 70.01
+married = True
+print(name, age,price)
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(married))
+
+name1="nameone"
+name2='nametwo'
+name3='''namethree'''
+
+print(name1)
+print(name2)
+print(name3)
+
+none = None
+
+print(type(none))
+#case sensitive
+
+'''
+multi
+line 
+comments
+'''
+apple = "apple"
+Apple = "banana"
+print(apple)
+print(Apple)

7ea8cfcc1dc83719eb6eeeb19414d4f9bcab1616
vikrammthontanaal <thontanaalvikram@gmail.com>
2025-03-19 10:28:53 +0530

    lecture 2

---
+++ lecturetwo.py
@@ -0,0 +1,17 @@
+str1 = "this is vikram,\tand hello world"
+print(str1)
+#\t, \n
+
+str2 ="vikram"
+str3="t"
+print(str2+" "+str3)
+print(len(str2+str3))
+
+str4="this is vikram"
+print(str4[3])
+
+print(str4[1:4])
+print(str4[8:len(str4)])
+print(str4[8:])
+print(str4[:4])
+print(str4[-6:-1])
